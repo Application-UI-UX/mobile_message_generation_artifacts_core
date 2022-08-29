8d11f4a9ed077fde7e6d206dfa1bd5a088611db8
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-28 23:22:30 -0400

    message testing

--- catkin_generated/installspace/rosjava_messagesConfig.cmake
+++ catkin_generated/installspace/rosjava_messagesConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /usr/local/lib;/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /usr/local/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
