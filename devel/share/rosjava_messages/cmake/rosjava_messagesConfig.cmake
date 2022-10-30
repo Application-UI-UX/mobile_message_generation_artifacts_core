65684c31351c24e8a7f1601db747f0108c4e8a6f
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-10-30 19:13:00 -0400

    repo plug

--- devel/share/rosjava_messages/cmake/rosjava_messagesConfig.cmake
+++ devel/share/rosjava_messages/cmake/rosjava_messagesConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel/lib;/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
