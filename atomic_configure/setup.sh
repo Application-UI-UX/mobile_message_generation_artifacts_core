722cdaa7a106f8f3bf79fe7864b73adeda56e36e
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-11-14 12:58:49 -0500

    communication

--- atomic_configure/setup.sh
+++ atomic_configure/setup.sh
@@ -11,7 +11,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel}
+: ${_CATKIN_SETUP_DIR:=/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
--- catkin_generated/generate_cached_setup.py
+++ catkin_generated/generate_cached_setup.py
@@ -12,16 +12,16 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel'.split(';'):
+    for workspace in '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel;/opt/ros/noetic'.split(';'):
         python_path = os.path.join(workspace, 'lib/python3/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
             break
     from catkin.environment_cache import generate_environment_script
 
-code = generate_environment_script('/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/devel/env.sh')
+code = generate_environment_script('/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel/env.sh')
 
-output_filename = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_messages/catkin_generated/setup_cached.sh'
+output_filename = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/catkin_generated/setup_cached.sh'
 with open(output_filename, 'w') as f:
     # print('Generate script for cached setup "%s"' % output_filename)
     f.write('\n'.join(code))
