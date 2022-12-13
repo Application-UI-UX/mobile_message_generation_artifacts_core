bc71115bb18d360cef1f1cf4ee5078588b82b709
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-12-12 19:42:47 -0500

    recal

--- catkin_generated/setup_cached.sh
+++ catkin_generated/setup_cached.sh
@@ -5,5 +5,10 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export CMAKE_PREFIX_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel"
+export LD_LIBRARY_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel/lib"
+export PKG_CONFIG_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages/devel/lib/pkgconfig"
+export PWD="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosmobile_messages"
+export PYTHONPATH=""
 
 # modified environment variables
